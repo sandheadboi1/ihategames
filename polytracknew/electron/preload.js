81de40ff0d871fedefe66d01976dd2f446941e10
blobbo7 <jssthurston@gmail.com>
2025-09-09 08:41:30 -0600

    dih

---
+++ polytracknew/electron/preload.js
@@ -0,0 +1,1 @@
+const{contextBridge,ipcRenderer}=require("electron");contextBridge.exposeInMainWorld("electron",{quit:()=>ipcRenderer.send("quit"),addFullscreenChangeListener:e=>ipcRenderer.on("fullscreen-change",(()=>e())),isFullscreen:()=>ipcRenderer.sendSync("is-fullscreen"),setFullscreen:e=>ipcRenderer.send("set-fullscreen",e)});
